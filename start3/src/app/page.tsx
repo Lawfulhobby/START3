4a05e5bd872f29edc59436df188eb4f89d6bfcda
blessing <blessinghove69@gmail.com>
2024-09-20 21:05:15 +0200

    Added GridCards

--- start3/src/app/page.tsx
+++ start3/src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { FlyoutNavbar } from '@/components/FlyoutNavbar'
+import { GridCards } from '@/components/GridCards'
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -25,25 +26,12 @@ export default function Example() {
         <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
             <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
-              We’re changing the way people connect.
+              Welcome to Start3: Your Web3 Journey Begins
             </h1>
             <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
               <p className="text-lg leading-8 text-gray-600">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                fugiat veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo.
+                At Start3, we empower newcomers to navigate the exciting world of web3 with ease. Our mission is to simplify blockchain technologies, making them accessible and engaging for everyone.
               </p>
-              <div className="mt-10 flex items-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Get started
-                </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                  Learn more <span aria-hidden="true">→</span>
-                </a>
-              </div>
             </div>
             <img
               alt=""
@@ -53,6 +41,7 @@ export default function Example() {
           </div>
         </div>
         <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
+        <GridCards/>
       </div>
     </div>
   )
