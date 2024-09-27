4e5f3fb09aca775b23250a02c878b0f633618b3b
blessing <blessinghove69@gmail.com>
2024-09-27 22:30:07 +0200

    Initialize Base Buildathon repo

--- start3/src/app/providers.tsx
+++ start3/src/app/providers.tsx
@@ -1,36 +1,66 @@
+// "use client";
+
+// import { WagmiProvider, cookieToInitialState } from "wagmi";
+// import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// import { config } from "@/lib/config";
+
+// const queryClient = new QueryClient();
+
+// type Props = {
+//   children: React.ReactNode;
+//   cookie?: string | null;
+// };
+
+// export default function Providers({ children, cookie }: Props) {
+//   const initialState = cookieToInitialState(config, cookie);
+
+//   return (
+//     <WagmiProvider config={config} initialState={initialState}>
+//       <QueryClientProvider client={queryClient}>
+//         <RainbowKitProvider
+//           theme={darkTheme({
+//             accentColor: "#0E76FD",
+//             accentColorForeground: "white",
+//             borderRadius: "large",
+//             fontStack: "system",
+//             overlayBlur: "small",
+//           })}
+//         >
+//           {children}
+//         </RainbowKitProvider>
+//       </QueryClientProvider>
+//     </WagmiProvider>
+//   );
+// }
 "use client";
-
-import { WagmiProvider, cookieToInitialState } from "wagmi";
-import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { ReactNode } from 'react';
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { baseSepolia } from 'wagmi/chains';
+import { coinbaseWallet } from 'wagmi/connectors';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import { config } from "@/lib/config";
 
 const queryClient = new QueryClient();
 
-type Props = {
-  children: React.ReactNode;
-  cookie?: string | null;
-};
+const wagmiConfig = createConfig({
+  chains: [baseSepolia],
+  connectors: [
+    coinbaseWallet({
+      appName: 'onchainkit',
+    }),
+  ],
+  ssr: true,
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
 
-export default function Providers({ children, cookie }: Props) {
-  const initialState = cookieToInitialState(config, cookie);
-
+export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <WagmiProvider config={config} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          theme={darkTheme({
-            accentColor: "#0E76FD",
-            accentColorForeground: "white",
-            borderRadius: "large",
-            fontStack: "system",
-            overlayBlur: "small",
-          })}
-        >
-          {children}
-        </RainbowKitProvider>
+    <WagmiProvider config={wagmiConfig}>
+       <QueryClientProvider client={queryClient}>
+      {children}
       </QueryClientProvider>
-    </WagmiProvider>
-  );
+    </WagmiProvider>);
 }
--- start3/src/components/connectButton.tsx
+++ start3/src/components/connectButton.tsx
@@ -1,86 +1,135 @@
-"use client";
+// "use client";
 
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
+// import {
+//     useConnectModal,
+//     useAccountModal,
+//     useChainModal,
+// } from "@rainbow-me/rainbowkit";
+// import { useAccount, useDisconnect } from "wagmi";
+// import { emojiAvatarForAddress } from "@/lib/emojiAvatarForAddress";
+// import { Button } from "./ui/button";
+
+// export const ConnectBtn = () => {
+//     const { isConnecting, address, isConnected, chain } = useAccount();
+//     const { color: backgroundColor, emoji } = emojiAvatarForAddress(
+//         address ?? ""
+//     );
+
+//     const { openConnectModal } = useConnectModal();
+//     const { openAccountModal } = useAccountModal();
+//     const { openChainModal } = useChainModal();
+//     const { disconnect } = useDisconnect();
+
+//     const isMounted = useRef(false);
+
+//     useEffect(() => {
+//         isMounted.current = true;
+//     }, []);
+
+//     if (!isConnected) {
+//         return (
+//             <div className="flex justify-center">
+//                 <button
+//                  className="flex justify-center items-center px-4 py-3 border border-neutral-700 bg-neutral-800/30 rounded-xl font-mono font-bold gap-x-2 cursor-pointer"
+//                     onClick={async () => {
+//                         // Disconnecting wallet first because sometimes when is connected but the user is not connected
+//                         if (isConnected) {
+//                             disconnect();
+//                         }
+//                         openConnectModal?.();
+//                     }}
+//                     disabled={isConnecting}
+//                 >
+//                     {isConnecting ? "Connecting..." : "Connect your wallet"}
+//                 </button>
+//             </div>
+//         );
+//     }
+
+//     if (isConnected && !chain) {
+//         return (
+//             <div className="flex justify-center">
+//                 <button className="btn" onClick={openChainModal}>
+//                     Wrong network
+//                 </button>
+//             </div>
+//         );
+//     }
+
+//     return (
+//         <div className="max-w-5xl w-full flex items-center justify-center space-x-4">
+//             <div
+//                 className="flex justify-center items-center px-4 py-2 border border-neutral-700 bg-neutral-800/30 rounded-xl font-mono font-bold gap-x-2 cursor-pointer"
+//                 onClick={async () => openAccountModal?.()}
+//             >
+//                 <div
+//                     role="button"
+//                     tabIndex={1}
+//                     className="h-8 w-8 rounded-full flex items-center justify-center flex-shrink-0 overflow-hidden"
+//                     style={{
+//                         backgroundColor,
+//                         boxShadow: "0px 2px 2px 0px rgba(81, 98, 255, 0.20)",
+//                     }}
+//                 >
+//                     {emoji}
+//                 </div>
+//                 <p>Account</p>
+//             </div>
+//             <button
+//                 className="flex justify-center items-center px-4 py-3 border border-neutral-700 bg-neutral-800/30 rounded-xl font-mono font-bold gap-x-2 cursor-pointer"
+//                 onClick={openChainModal}>
+//                 Switch Networks
+//             </button>
+//         </div>
+//     );
+// };
+"use client"
 import {
-    useConnectModal,
-    useAccountModal,
-    useChainModal,
-} from "@rainbow-me/rainbowkit";
-import { useAccount, useDisconnect } from "wagmi";
-import { emojiAvatarForAddress } from "@/lib/emojiAvatarForAddress";
-import { Button } from "./ui/button";
+    ConnectWallet,
+    Wallet,
+    WalletDropdown,
+    WalletDropdownBasename,
+    WalletDropdownDisconnect,
+    WalletDropdownFundLink,
+    WalletDropdownLink,
+} from '@coinbase/onchainkit/wallet';
+import {
+    Address,
+    Avatar,
+    Name,
+    Identity,
+    EthBalance,
+} from '@coinbase/onchainkit/identity';
+import { color } from '@coinbase/onchainkit/theme';
 
 export const ConnectBtn = () => {
-    const { isConnecting, address, isConnected, chain } = useAccount();
-    const { color: backgroundColor, emoji } = emojiAvatarForAddress(
-        address ?? ""
-    );
-
-    const { openConnectModal } = useConnectModal();
-    const { openAccountModal } = useAccountModal();
-    const { openChainModal } = useChainModal();
-    const { disconnect } = useDisconnect();
-
-    const isMounted = useRef(false);
-
-    useEffect(() => {
-        isMounted.current = true;
-    }, []);
-
-    if (!isConnected) {
-        return (
-            <div className="flex justify-center">
-                <button
-                 className="flex justify-center items-center px-4 py-3 border border-neutral-700 bg-neutral-800/30 rounded-xl font-mono font-bold gap-x-2 cursor-pointer"
-                    onClick={async () => {
-                        // Disconnecting wallet first because sometimes when is connected but the user is not connected
-                        if (isConnected) {
-                            disconnect();
-                        }
-                        openConnectModal?.();
-                    }}
-                    disabled={isConnecting}
-                >
-                    {isConnecting ? "Connecting..." : "Connect your wallet"}
-                </button>
-            </div>
-        );
-    }
-
-    if (isConnected && !chain) {
-        return (
-            <div className="flex justify-center">
-                <button className="btn" onClick={openChainModal}>
-                    Wrong network
-                </button>
-            </div>
-        );
-    }
-
     return (
-        <div className="max-w-5xl w-full flex items-center justify-center space-x-4">
-            <div
-                className="flex justify-center items-center px-4 py-2 border border-neutral-700 bg-neutral-800/30 rounded-xl font-mono font-bold gap-x-2 cursor-pointer"
-                onClick={async () => openAccountModal?.()}
-            >
-                <div
-                    role="button"
-                    tabIndex={1}
-                    className="h-8 w-8 rounded-full flex items-center justify-center flex-shrink-0 overflow-hidden"
-                    style={{
-                        backgroundColor,
-                        boxShadow: "0px 2px 2px 0px rgba(81, 98, 255, 0.20)",
-                    }}
+        <Wallet>
+            <ConnectWallet>
+                <Avatar className="h-6 w-6" />
+                <Name />
+            </ConnectWallet>
+            <WalletDropdown>
+                <Identity
+                    className="px-4 pt-3 pb-2"
+                    hasCopyAddressOnClick
                 >
-                    {emoji}
-                </div>
-                <p>Account</p>
-            </div>
-            <button
-                className="flex justify-center items-center px-4 py-3 border border-neutral-700 bg-neutral-800/30 rounded-xl font-mono font-bold gap-x-2 cursor-pointer"
-                onClick={openChainModal}>
-                Switch Networks
-            </button>
-        </div>
+                    <Avatar />
+                    <Name />
+                    <Address />
+                    <EthBalance />
+                </Identity>
+                <WalletDropdownBasename />
+                <WalletDropdownLink
+                    icon="wallet"
+                    href="https://keys.coinbase.com"
+                >
+                    Wallet
+                </WalletDropdownLink>
+                <WalletDropdownFundLink />
+                <WalletDropdownDisconnect />
+            </WalletDropdown>
+        </Wallet>
     );
-};
+}
